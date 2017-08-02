f4c311b729b4ebfc05a6d7838f6684cc245e8e7d
Ludwig Wilke <ludwig.wilke98@gmail.com>
2017-08-02 12:19:00 +0200

    lay out styling, start js

---
+++ resources/js/match-game.js
@@ -0,0 +1,38 @@
+var MatchGame = {};
+
+/*
+  Sets up a new game after HTML document has loaded.
+  Renders a 4x4 board of cards.
+*/
+
+/*
+  Generates and returns an array of matching card values.
+ */
+
+MatchGame.generateCardValues = function () {
+  var numbers = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
+  var random = numbers[Math.floor(Math.random() * numbers.length)];
+  return random;
+  var unplaced= [];
+  for (i = 1; i < numbers.length; i++) {
+    unplaced.push(random,random);
+  }
+};
+
+/*
+  Converts card values to jQuery card objects and adds them to the supplied game
+  object.
+*/
+
+MatchGame.renderCards = function(cardValues, $game) {
+
+};
+
+/*
+  Flips over a given card and checks to see if two cards are flipped over.
+  Updates styles on flipped cards depending whether they are a match or not.
+ */
+
+MatchGame.flipCard = function($card, $game) {
+
+};
